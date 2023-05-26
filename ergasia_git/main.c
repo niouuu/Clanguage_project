b2bac2a02f7722570a6e11f3b459d4acd61bd62a
Antwniou Nikolaos <nikos.niouu@gmail.com>
2023-05-26 21:20:28 +0300

    Initial Commit

---
+++ ergasia_git/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  ergasia_git
+//
+//  Created by NIKOS ANTWNIOU on 26/5/23.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
