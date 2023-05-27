7d008f7a79c75f25644c4f7435332c369d23a72f
Antwniou Nikolaos <nikos.niouu@gmail.com>
2023-05-27 18:00:11 +0300

    The beggining of a pizza delivery project.In my first commit i created the threads and the order function.

--- ergasia_git/main.c
+++ ergasia_git/main.c
@@ -6,9 +6,52 @@
 //
 
 #include <stdio.h>
+#include <pthread.h>
+#include "metavlhtes.h"
+#include <stdlib.h>
+#include<unistd.h>
+#include <time.h>
+#include <stdbool.h>
+#include <math.h>
 
-int main(int argc, const char * argv[]) {
+void *order(void *x){
+    
+    
+    int id_nhma=*(int *)x;
+    printf("Order  %d just started \n",id_nhma);
+    pthread_mutex_lock(&lock);
+    
+    
+    return 0;
+}
+
+int main(int argc, const char * argv[]){
     // insert code here...
-    printf("Hello, World!\n");
+    
+    int resources;
+    //Customers and random initialization
+    Ncust=atoi(argv[1]);
+    random1=atoi(argv[2]);
+   
+
+    pthread_t threads[Ncust];//Array for threads used for customers
+    pthread_mutex_init(&lock, NULL);//initialize mutex
+    pthread_cond_init(&cond, NULL);//initialize condition
+    int id[Ncust];//Array for orders id
+
+    
+    for (int i=0; i<Ncust;i++) {
+        id[i]=i+1;
+        printf("Thread with id %d, created\n",i+1);
+        
+
+        resources=pthread_create(&threads[i], NULL, order, &id[i]);//thread creation and order function called through pthread_create
+        
+        
+
+    }
+    for (int i = 0; i < Ncust; i++) {
+        pthread_join(threads[i], NULL);
+    }
     return 0;
 }
