f13e9b3a4e6c8f1adc6a262312fc6d162ef57dac
Antwniou Nikolaos <nikos.niouu@gmail.com>
2023-05-30 00:43:26 +0300

    In this commit, i created a possibility for what each pizza that the customer ordered is.

--- ergasia_git/main.c
+++ ergasia_git/main.c
@@ -13,6 +13,9 @@
 #include <time.h>
 #include <stdbool.h>
 #include <math.h>
+unsigned int* random_number_seed(void){
+   return &random1;
+}
 
 void *order(void *x){
     
@@ -20,7 +23,26 @@ void *order(void *x){
     int id_nhma=*(int *)x;
     printf("Order  %d just started \n",id_nhma);
     pthread_mutex_lock(&lock);
-    
+    // Each time a new customer(thread) starts ordering, we use a new probability to which type each pizza will be
+    //thats why i created a function called random_number_seed
+    unsigned int random_number_pizza=rand_r(random_number_seed())%Norderhigh+Norderlow;
+    int pizzas[random_number_pizza];
+    //analoga me to poses pitses thelei,briskoume poies einai aples kai poies special
+    for (int i=0; i<random_number_pizza; i++) {
+        
+        float possibility_plain=(float)rand_r(random_number_seed())/RAND_MAX;
+        //based on what i was given from my university,i created one if,because if the random possibility created is smaller or equal to 0.6 the pizza is plain and if not is special
+        if(possibility_plain<=0.6){
+            pizzas[i]=0;//plain
+            
+            
+        }else{
+            pizzas[i]=1;//special
+            
+        }
+        
+        
+    }
     
     return 0;
 }
